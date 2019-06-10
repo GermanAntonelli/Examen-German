a8c0a71ada89463b1fd4e29112b7608a94bcf99a
German <German@DESKTOP-JDLKDN3>
2019-06-10 13:47:27 -0600

    finalizacion de la parte visual del programa

--- German Examen Programacion de negocios/ClaseEmpleado.cs
+++ German Examen Programacion de negocios/ClaseEmpleado.cs
@@ -8,7 +8,7 @@ namespace German_Examen_Programacion_de_negocios
 {
     class Clase_Empleado
     {
-        string nombre_empleado;
+        string nombreempleado;
         string identidad;
         int horastrabajadas;
         decimal pagoporhoranormaldeltrabajo;
@@ -18,23 +18,23 @@ namespace German_Examen_Programacion_de_negocios
         //constructor por defecto
         public Clase_Empleado()
         {
-            nombre_empleado = "German Antonelli";
+            nombreempleado = "German Antonelli";
             identidad = "0318199901720";
             horastrabajadas = 8;
-            pagoporhoranormaldeltrabajo = 1200;
+            pagoporhoranormaldeltrabajo = 1500;
             horasextrastrabajadas = 6;
-            pagoporhoradetrabajoextra = 1200;
+            pagoporhoradetrabajoextra = 1500;
         }
 
-        public string Nombre_Empleado
+        public string NombreEmpleado
         {
             get
             {
-                return nombre_empleado;
+                return nombreempleado;
             }
             set
             {
-                nombre_empleado = value;
+                nombreempleado = value;
             }
         }
 
